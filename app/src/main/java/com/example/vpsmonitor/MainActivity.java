b16d8ca3cd48289bbd9f39c97ead70a35161d86a
Radhey Radhey <bibekmondal906@gmail.com>
2025-09-06 21:42:36 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/vpsmonitor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.vpsmonitor;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello VPS Monitor!");
+        setContentView(tv);
+    }
+}
