c5879d9d34937aae4e747f7d058e61fc7381918a
Radhey Radhey <bibekmondal906@gmail.com>
2025-09-06 22:32:13 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,10 +1,5 @@
-@echo off
-set DIR=%~dp0
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIR%
-
-set DEFAULT_JVM_OPTS=
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-java %DEFAULT_JVM_OPTS% -classpath %CLASSPATH% org.gradle.wrapper.GradleWrapperMain %*
+@ECHO OFF
+SET DIR=%~dp0
+SET APP_HOME=%DIR%
+SET CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
