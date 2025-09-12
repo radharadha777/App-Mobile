64c5c4a92b681c8c2838d889c528f1528f8823dc
Radha Radha <bibekmondal906@gmail.com>
2025-09-12 08:47:42 +0530

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET DIR=%~dp0
-SET APP_HOME=%DIR%
-SET CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
-"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
